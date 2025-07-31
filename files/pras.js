d0ba0df3c3ce0b2111c1d251ce4472f579296047
prasannathiyagarajan <prasannamca.t@gmail.com>
2025-07-30 23:02:20 -0400

    Initial Commit

--- files/pras.js
+++ files/pras.js
@@ -1,6 +1,13 @@
-import { readFile } from 'node:fs';
+const fs = require('fs');
+const path = require('path');
 
-readFile('./files/madhhu.txt', (err, data) => {
+fs.readFile(path.join(__dirname, 'madhhu.txt'), 'utf8', (err, data) => {
   if (err) throw err;
-  console.log(data.toString());
-}); 
+  console.log(data);
+});
+console.log("Hi")
+//to catch the uncaught error 
+process.on('uncaughtException', (err) => {
+  console.error('There was an uncaught error', err);
+  // Perform any necessary cleanup or logging here
+});
