d90c61a86ac4a41d0f042e2f52a68a3777935ba7
madhupras <madhu181988@gmail.com>
2025-07-30 23:47:34 -0400

    Initial Commit

--- files/pras.js
+++ files/pras.js
@@ -5,6 +5,10 @@ fs.readFile(path.join(__dirname, 'madhhu.txt'), 'utf8', (err, data) => {
   if (err) throw err;
   console.log(data);
 });
+fs.writeFile(path.join(__dirname, 'tulasi.txt'),'Happy Birthday Dear' ,'utf8', (err) => {
+  if (err) throw err;
+  console.log('write operation completed');
+});
 console.log("Hi")
 //to catch the uncaught error 
 process.on('uncaughtException', (err) => {
